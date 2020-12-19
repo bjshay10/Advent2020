875461e26f6be79ca74b5651b34d34b5db32fb5d
bjshay10 <bj.shay@d51schools.org>
2020-12-19 10:35:27 -0700

    day 8 finished

--- Day8/8_2.js
+++ Day8/8_2.js
@@ -3,6 +3,20 @@ var fs = require('fs')
 var accumulator = 0
 var instructionRan = []
 var accumulatorValues = []
+var jmpNopArray = []
+
+const listJmpNop = ((jmpnopArray) => {
+    for (b in jmpnopArray) {
+        var tempjmpnop = jmpnopArray[b].toString().split(' ')
+        var tempInstJmpNop = tempjmpnop[0].toString()
+        if (tempInstJmpNop === 'jmp'){
+            jmpNopArray.push(b)
+        } else if (tempInstJmpNop === 'nop'){
+            jmpNopArray.push(b)
+        }
+    }
+    return
+})
 
 const wasInstRan = ((instIndex) => {
     // console.log(instIndex)
@@ -20,13 +34,15 @@ const wasInstRan = ((instIndex) => {
     return 'No'
 })
 
-const parseInstructions = ((instArray) => {
+const parseInstructions = ((instArray, switchIndx) => {
     // for (a in array) {
     var indx = 0
     var status = 'working'
+    instructionRan = []
 
     do {
 
+        console.log(accumulator)
         //console.log(`Current Instruction: ${instArray[indx]}`)
         
         //check to see if index is in instructionRan
@@ -37,6 +53,8 @@ const parseInstructions = ((instArray) => {
             status = 'done'
             break;
         }
+
+        
         
         //get  acc, jmp, or nop
         var temp = instArray[indx].toString().split(' ')
@@ -45,6 +63,18 @@ const parseInstructions = ((instArray) => {
         var tempAmnt = temp[1].toString().substring(1)
         //console.log(`Instruction is '${tempInst}', + or - '${tempPM}', amount = '${tempAmnt}'`)
         instructionRan.push(indx)
+        
+        if (parseInt(indx) == parseInt(switchIndx)){
+            // console.log(`was ${tempInst}`)
+            if (tempInst === 'jmp') {
+                tempInst = 'nop'
+            } else if (tempInst === 'nop') {
+                tempInst = 'jmp'
+            }
+            // console.log(`is ${tempInst}`)
+        }
+
+
         switch(tempInst) {
             case 'acc':
                 //console.log(`accumulator start = ${accumulator}`)
@@ -72,8 +102,11 @@ const parseInstructions = ((instArray) => {
                 indx++
                 break;
         }
-     
-
+        
+        
+        if (indx == 636){
+            return 'finished'
+        }
     } while (status !== 'done')
 
     return status
@@ -82,17 +115,24 @@ const parseInstructions = ((instArray) => {
 
 fs.readFile('./Day8/input.txt', function(err, data) {
     // fs.readFile('./Day8/input-test.txt', function(err, data) {
+    accumulator = 0
+
     if(err) throw err
 
     var array = data.toString().split('\r\n')
 
-    //get a list of jmp indexs??
-    //loop through the list to switch to nop
+    listJmpNop(array)
+    for (c in jmpNopArray) {
+        accumulator = 0
+        const finalInst = parseInstructions(array, jmpNopArray[c])
+        // console.log(`c= ${c} and finalInst = ${finalInst}`)
+        if (finalInst == 'finished') {
+            console.log(`accumulator == ${accumulator}`)
+            break
+        }
+    }
     
-
-
-    const finalInst = parseInstructions(array)
-
-    console.log(`accumulator = ${accumulator}`)
+    
+    // console.log(`accumulator = ${accumulator}`)
     // console.log(accumulatorValues)
 })
